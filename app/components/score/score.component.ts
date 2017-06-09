237c34ed9c56ac84248b8aacd24bfb17214cab85
MunduriRachanaSree <mundurirachana@gmail.com>
2017-06-09 22:53:17 +0530

    satisfing requirements

--- app/components/score/score.component.ts
+++ app/components/score/score.component.ts
@@ -22,7 +22,7 @@ import 'rxjs/add/operator/filter';
             <p class="tries tries--correct">Correct Notes: {{ scoreTracker.totalCorrect }} of {{ scoreTracker.notesLimit }}</p>
             <p class="tries tries--incorrect">Incorrect Notes: {{ scoreTracker.totalIncorrect }} of {{ scoreTracker.notesLimit }}</p>
             <ul>
-                <li *ngFor="#note of notes">{{ note.correct }} answer: {{ note.genKey }} {{note.genType}}</li>
+                <li *ngFor="#note of notes">{{ note.correct }}<span *ngIf ="note.correct != true"> answer: {{ note.genKey }} {{note.genType}}</span></li>
             </ul>
         </div>
     </div>
